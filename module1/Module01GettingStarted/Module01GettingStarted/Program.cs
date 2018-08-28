4793edbdae8be1bf27e8346d4f53941f8e730ee5
Bryan Fraser <bryan.d.fraser@gmail.com>
2018-08-27 20:58:50 -0500

    Module1

---
+++ module1/Module01GettingStarted/Module01GettingStarted/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Module01GettingStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
