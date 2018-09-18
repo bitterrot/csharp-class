b2c695f1f534e76d701fd41afafd055b7e6b9979
Bryan Fraser <bryan.d.fraser@gmail.com>
2018-09-17 19:46:08 -0500

    module4
    Module 4

---
+++ module4/ConvertQuartersToDollars/ConvertQuartersToDollars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConvertQuartersToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many quarters? ");
+            int quarters = int.Parse(Console.ReadLine());
+            ConvertToDollars(quarters);
+            Console.ReadLine();
+        }
+
+        public static void ConvertToDollars(int quarters)
+        {
+            Console.WriteLine("You have {0} dollars and {1} cents.", quarters / 4, (quarters % 4) * 25);
+        }
+    }
+}
---
+++ module4/PaintingEstimate/PaintingEstimate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PaintingEstimate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of wall (in ft.): ");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Enter the width of the wall (in ft.): ");
+            double width = double.Parse(Console.ReadLine());
+            double wallArea = ComputeWallArea(length, width);
+            double paintCost = ComputePaintPrice(wallArea);
+            double laborCost = ComputeLaborPrice(wallArea);
+            Console.WriteLine("The total cost to paint the room is {0:C}", paintCost + laborCost);
+            Console.ReadLine();
+        }
+
+        public static double ComputeWallArea(double length, double width)
+        {
+            double area = length * width * 4 - (6 * 4);
+            return area;
+        }
+
+        public static double ComputePaintPrice(double area)
+        {
+            double paintCost = area * 1.5;
+            return paintCost;
+        }
+
+        public static double ComputeLaborPrice(double area)
+        {
+            double laborCost = area * 6;
+            return laborCost;
+        }
+    }
+}
